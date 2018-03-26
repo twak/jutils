00ad41e4f2f561cad9ae86ee858a923503ba11bc
twakelly@gmail.com <twakelly@gmail.com>
2018-03-26 13:55:55 +0100

    blur fns!

--- src/org/twak/utils/Imagez.java
+++ src/org/twak/utils/Imagez.java
@@ -1,9 +1,11 @@
 package org.twak.utils;
 
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.imageio.IIOImage;
@@ -63,6 +65,111 @@ public class Imagez {
 		} catch ( Throwable th ) {
 			th.printStackTrace();
 		}
+	}
+	
+	
+	private static Cache<Integer, float[][]> gaussian = new Cache<Integer, float[][]>() {
+
+		@Override
+		public float[][] create( Integer radius ) {
+
+			int size = radius * 2 + 1;
+			
+	        float[] data = new float[size];
+	        
+	        float sigma = radius / 3.0f;
+	        float twoSigmaSquare = 2.0f * sigma * sigma;
+	        float sigmaRoot = (float) Math.sqrt(twoSigmaSquare * Math.PI);
+	        float total = 0.0f;
+	        
+	        for (int i = -radius; i <= radius; i++) {
+	            float distance = i * i;
+	            int index = i + radius;
+	            data[index] = (float) Math.exp(-distance / twoSigmaSquare) / sigmaRoot;
+	            total += data[index];
+	        }
+	        
+	        for (int i = 0; i < data.length; i++) {
+	            data[i] /= total;
+	        }        
+	        
+	        float[][] d2 = new float[size][size];
+	        for (int x = 0; x < data.length; x++) 
+	        	for (int y = 0; y < data.length; y++) {
+	        		d2[x][y] = data[x] * data[y];
+	        	}
+	        
+	        return d2;
+		}
+		
+	};
+
+	private static void toComp( int c, double[] t2, double scale ) {
+		t2[0] +=  ( ( c >> 16 ) & 0xFF ) * scale;
+		t2[1] +=  ( ( c >> 8  ) & 0xFF ) * scale;
+		t2[2] +=  ( ( c       ) & 0xFF ) * scale;
+	}
+	
+    public static BufferedImage blur(int radius, BufferedImage in ) {
+        
+    	if (radius < 1) 
+        	return in;
+        
+		int size = radius * 2 + 1;
+        float[][] filter = gaussian.get(radius);
+        
+        double[] accm = new double[4];
+        BufferedImage out = new BufferedImage( in.getWidth(), in.getHeight(), BufferedImage.TYPE_3BYTE_BGR );
+        
+        for (int xs = 0; xs < in.getWidth(); xs++)
+        	for (int ys = 0; ys < in.getHeight(); ys++) {
+        		
+        		Arrays.fill( accm, 0 );
+        		
+        		for (int xf = 0; xf < size; xf++)
+        			for (int yf = 0; yf < size; yf ++) {
+        				
+        				int px = Mathz.clamp (xs + xf - radius, 0, in.getWidth ()-1);
+        				int py = Mathz.clamp (ys + yf - radius, 0, in.getHeight()-1);
+        				
+        				toComp (in.getRGB( px, py ), accm, filter[xf][yf] );
+        			}
+        		
+        		out.setRGB( xs, ys,
+        				0xff000000+
+        				(((int) accm[0]) << 16) + 
+        				(((int) accm[1]) <<  8) + 
+        				(((int) accm[2]) <<  0)  );
+        	}
+        
+        return out;
+    }
+
+	public static BufferedImage scaleLongest (BufferedImage in, int longest) {
+		
+		
+		double scale; 
+		
+		if (in.getWidth() > in.getHeight()) {
+			scale = longest / (double) in.getWidth() ;
+		}
+		else {
+			scale = longest / (double) in.getHeight();
+		}
+	
+		BufferedImage out = new BufferedImage( 
+				(int) (in.getWidth() * scale), 
+				(int) (in.getHeight() * scale),
+				BufferedImage.TYPE_3BYTE_BGR );
+		
+		Graphics2D  g = out.createGraphics();
+		g.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR );
+//	    g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+		g.drawImage( in, 0, 0, out.getWidth(), out.getHeight(), 0, 0, in.getWidth(), in.getHeight(), null );
+		g.dispose();
+		
+		return out;
 	}
 
 }
