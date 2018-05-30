8c4f08b2fdeb8413794593bd660c81aeb03a9ca7
twak <twakelly@gmail.com>
2018-05-30 01:01:02 +0100

    b

--- src/org/twak/utils/Imagez.java
+++ src/org/twak/utils/Imagez.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
